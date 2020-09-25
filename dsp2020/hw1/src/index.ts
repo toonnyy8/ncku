0725823816e9940089c4247692a17bc91f1a2750
toonnyy8 <toonnyy8@gmail.com>
2020-09-25 16:17:41 +0800

    update

--- dsp2020/hw1/src/index.ts
+++ dsp2020/hw1/src/index.ts
@@ -11,6 +11,10 @@ import * as tf from "@tensorflow/tfjs"
     console.log(
         new Array(40).fill(0).map((_, idx) => f(idx))
     )
+    const x = (n: number) => {
+        if (n == 0) return 1
+        else return 0
+    }
 
     const g = (x: (n: number) => number) => {
         const memory: number[] = []
@@ -23,7 +27,7 @@ import * as tf from "@tensorflow/tfjs"
         return _g
     }
 
-    const _g = g(f)
+    const _g = g(x)
     console.log(
         new Array(100).fill(0).map((_, idx) => _g(idx))
     )
@@ -38,5 +42,35 @@ import * as tf from "@tensorflow/tfjs"
 }
 
 {
-    const filter = (b: number[], a: number[]) => (x) => { }
+    const filter = (b: number[], a: number[], x: number[]) => {
+        if (a[0] != 1) {
+            console.error("a[0] must equal 1")
+            return
+        }
+        const arrfn = (arr: number[]) => {
+            return (n: number) => {
+                if (arr[n] != undefined) return arr[n]
+                else return 0
+            }
+        }
+        const _b = arrfn(b)
+        const _a = arrfn(a)
+        const _x = arrfn(x)
+        const memory: number[] = []
+        const f = (n) => {
+            if (n < 0) return 0
+            else if (memory.length > n) return memory[n]
+            else {
+                let out = 0
+                for (let k = 0; k <= n; k++) {
+                    out += _x(n - k) * _b(k) - f(n - (k + 1)) * _a(k + 1)
+                }
+                memory[n] = out
+                return out
+            }
+        }
+        return f
+    }
+    let f = filter([1, 1 / 2], [1, -1.8 * Math.cos(Math.PI / 16), 0.81], [1])
+    console.log(new Array(40).fill(0).map((_, idx) => f(idx)))
 }
