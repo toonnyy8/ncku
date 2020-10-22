979d8bcf24f0be14132b24ca3b302c5822a4c894
toonnyy8 <toonnyy8@gmail.com>
2020-10-22 23:47:47 +0800

    update

--- dsp2020/hw2/dev/src/index.ts
+++ dsp2020/hw2/dev/src/index.ts
@@ -8,7 +8,7 @@ const ilpf = (T: number) => {
         return a == 0 && b == 0 ? 1 : a / b
     }
 }
-
+tf.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).reshape([-1, 5]).slice([0, 0], [-1, 1]).print()
 
 document.getElementById("upload").onclick = () => {
     let load = document.createElement("input")
@@ -22,119 +22,89 @@ document.getElementById("upload").onclick = () => {
             const audioContext = new AudioContext({ sampleRate: 16000 })
             audioContext
                 .decodeAudioData(<ArrayBuffer>reader.result)
-                .then(audioBuffer => audioBuffer.getChannelData(0))
+                .then(audioBuffer => {
+
+                    let source = audioContext.createBufferSource();
+                    // set the buffer in the AudioBufferSourceNode
+                    let bf0 = audioBuffer.getChannelData(0)
+                    bf0.reverse()
+                    let bf1 = audioBuffer.getChannelData(1)
+                    bf1.reverse()
+
+                    source.buffer = audioBuffer
+
+                    // connect the AudioBufferSourceNode to the
+                    // destination so we can hear the sound
+                    source.connect(audioContext.destination);
+
+                    // start the source playing
+                    source.start();
+
+                    return audioBuffer.getChannelData(0)
+                })
                 .then(bufferData => {
-                    // bufferData.reduce((prev,curr))
 
-                    const data = Array.from(bufferData.slice(100, 200))
-                    {
-                        const _data = data
-                            .slice(1)
-                            .reverse()
-                            .concat(data)
-                            .concat(data.slice(0, -1).reverse())
-                        const ilpfs = _data.map((val, idx) => {
-                            const f = ilpf(1)
-                            return (n: number) => f(n - idx) * val
-                        })
-                        const calc = (n: number) => ilpfs.reduce((prev, f) => prev + f(n), 0)
+                    // const _bufferData = bufferData.slice(0, Math.floor(bufferData.length / 2))
+                    // const _data = Array.from(_bufferData)
 
-                        console.log(data.reduce((prev, curr) => prev + curr, 0))
-                        console.log(calc(0))
-                        const chart = new g2.Chart({
-                            container: document.body,
-                            autoFit: false,
-                            width: 1200,
-                            height: 300,
-                        });
-                        const L = 3
-                        chart
-                            .data(
-                                [
-                                    ..._data
-                                        .map((_, idx) => new Array(L).fill(0)
-                                            .map((_, _idx) => calc(idx + _idx / L))
-                                        )
-                                        .slice(data.length - 1, data.length * 2 - 1)
-                                        .reduce((prev, curr) => prev.concat(curr), [])
-                                        .slice(0, -L + 1)
-                                        .map((val, idx) => ({ n: idx / L, value: val }))
-                                    // ...data.map((val, idx) => ({ n: idx, value: val }))
-                                ]
-                            )
-                            .line()
-                            .position('n*value')
-                        chart.render()
-                    }
+                    // const filterSize = 25
+                    // const L = 3
+                    // const M = 4
+                    // const kernel = tf.tensor3d(
+                    //     new Array(filterSize * 2 * 3 + 1)
+                    //         .fill(0)
+                    //         .map((_, idx) => ilpf(M / L)(idx / L - filterSize)),
+                    //     [filterSize * 2 * 3 + 1, 1, 1]
+                    // )
+                    // const data =
+                    //     (() => {
+                    //         let data = Array.from(_bufferData)
+                    //         data = data
+                    //             .slice(1, (filterSize + 1))
+                    //             .reverse()
+                    //             .concat(data)
+                    //             .concat(data
+                    //                 .slice(-(filterSize + 1), -1)
+                    //                 .reverse())
 
-                    {
-                        const _data = data
-                            .slice(1)
-                            .reverse()
-                            .concat(data)
-                            .concat(data.slice(0, -1).reverse())
-                        const chart = new g2.Chart({
-                            container: document.body,
-                            autoFit: false,
-                            width: 1200,
-                            height: 300,
-                        });
-                        const f = ilpf(4 / 3)
+                    //         let tensorData = tf.tensor2d(data, [data.length, 1])
 
+                    //         return tf.tidy(() => tf.concat(
+                    //             [
+                    //                 tensorData,
+                    //                 ...new Array(L - 1).fill(tf.zeros([data.length, 1]))
+                    //             ],
+                    //             -1)
+                    //             .reshape([-1, 1])
+                    //         )
+                    //     })()
+                    // let out: tf.Tensor = tf.tidy(() => tf.conv1d(data, kernel, 1, "valid").flatten())
+                    // let shortage = out.shape[0] % M
+                    // if (shortage != 0) {
+                    //     console.log(out)
+                    //     out = out.concat(tf.zeros([M - shortage]), 0)
+                    // }
 
+                    // out = out
+                    //     .reshape([-1, M])
+                    //     .slice([0, 0], [-1, 1])
+                    //     .flatten()
 
-                        const __data = tf.conv1d(
-                            tf.tensor2d(_data, [_data.length, 1]),
-                            tf.tensor3d(
-                                new Array(101)
-                                    .fill(0)
-                                    .map((_, idx) => f(idx - 50)),
-                                [101, 1, 1]),
-                            1,
-                            "same")
-                            .flatten()
-                            .arraySync()
-                            .slice(data.length - 1, data.length * 2 - 1)
+                    // const audioContextOut = new AudioContext({ sampleRate: 12000 })
+                    // let myArrayBuffer = audioContextOut.createBuffer(1, out.shape[0], audioContextOut.sampleRate * (L / M));
+                    // let bf = myArrayBuffer.getChannelData(0)
+                    // let obf = out.bufferSync()
+                    // bf.forEach((_, idx) => bf[idx] = obf[idx])
+                    // let source = audioContextOut.createBufferSource();
+                    // // set the buffer in the AudioBufferSourceNode
+                    // source.buffer = myArrayBuffer
 
-                        const ___data = __data
-                            .slice(1)
-                            .reverse()
-                            .concat(data)
-                            .concat(data.slice(0, -1).reverse())
+                    // // connect the AudioBufferSourceNode to the
+                    // // destination so we can hear the sound
+                    // source.connect(audioContextOut.destination);
 
-                        const ilpfs = ___data.map((val, idx) => {
-                            const f = ilpf(1)
-                            return (n: number) => f(n - idx) * val
-                        })
-                        const calc = (n: number) => ilpfs.reduce((prev, f) => prev + f(n), 0)
-                        const L = 3
-
-                        console.log(
-                            __data
-                                .map((_, idx) => new Array(L).fill(0)
-                                    .map((_, _idx) => calc(idx + _idx / L))
-                                )
-                        )
-
-                        chart
-                            .data(
-                                [
-                                    ...___data
-                                        .map((_, idx) => new Array(L).fill(0)
-                                            .map((_, _idx) => calc(idx + _idx / L))
-                                        )
-                                        .slice(data.length - 1, data.length * 2 - 1)
-                                        .reduce((prev, curr) => prev.concat(curr), [])
-                                        .slice(0, -L + 1)
-
-                                        .map((val, idx) => ({ n: idx / L, value: val }))
-                                    // ...data.map((val, idx) => ({ n: idx, value: val }))
-                                ]
-                            )
-                            .line()
-                            .position('n*value')
-                        chart.render()
-                    }
+                    // // start the source playing
+                    // source.start();
 
                 })
         })
@@ -142,78 +112,4 @@ document.getElementById("upload").onclick = () => {
         reader.readAsArrayBuffer(files[0])
     }
     load.click()
-}
-
-{
-    const f = ilpf(4 / 3)
-
-    const chart = new g2.Chart({
-        container: document.body,
-        autoFit: false,
-        width: 1200,
-        height: 300,
-    });
-
-    chart
-        .data(
-            [
-                ...new Array(2201).fill(0).map((_, idx) => ({ n: idx / 100, value: f((idx / 100)) }))
-            ]
-        )
-        .line()
-        .position('n*value')
-    chart.render()
-}
-
-{
-    const f = ilpf(1)
-
-    const chart = new g2.Chart({
-        container: document.body,
-        autoFit: false,
-        width: 1200,
-        height: 300,
-    });
-
-    chart
-        .data(
-            [
-                ...new Array(500)
-                    .fill(0)
-                    .map((_, idx) => f((idx / 10) - 25))
-                    .map((val, idx) => ({ n: idx / 10, value: val }))
-            ]
-        )
-        .line()
-        .position('n*value')
-    chart.render()
-}
-
-
-
-{
-    const f = ilpf(4 / 3)
-    console.log(new Array(101)
-        .fill(0)
-        .map((_, idx) => f(idx - 50)))
-
-    const chart = new g2.Chart({
-        container: document.body,
-        autoFit: false,
-        width: 1200,
-        height: 300,
-    });
-
-    chart
-        .data(
-            [
-                ...new Array(101)
-                    .fill(0)
-                    .map((_, idx) => f(idx - 50))
-                    .map((val, idx) => ({ n: idx, value: val }))
-            ]
-        )
-        .line()
-        .position('n*value')
-    chart.render()
 }
