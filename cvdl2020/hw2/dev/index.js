19e9cb5ba524d090ae8e541f94ee4199a9954f78
toonnyy8 <toonnyy8@gmail.com>
2020-12-12 01:41:13 +0800

    update

--- cvdl2020/hw2/dev/index.js
+++ cvdl2020/hw2/dev/index.js
@@ -2,10 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const Koa = require('koa');
 const cors = require('@koa/cors');
-const cv = require('opencv4nodejs')
+const cv2 = require('opencv4nodejs')
 
-let img = cv.imread(`${__dirname}/../Q4_Image/1.jpg`, cv.IMREAD_COLOR)
-console.log(img)
+let img = cv2.imread(`${__dirname}/../Q4_Image/1.jpg`, cv2.IMREAD_COLOR)
+
+let bgSub = new cv2.VideoCapture(`${__dirname}/../Q1_Image/bgSub.mp4`)
+/**
+ * @type {Buffer}
+ */
+let bgSubFrames
+let bgSubH = 0, bgSubW = 0, bgSubFrameNum = 0
+{
+    let frame = bgSub.read().cvtColor(cv2.COLOR_BGR2GRAY)
+    bgSubFrames = frame.getData()
+    bgSubH = frame.rows
+    bgSubW = frame.cols
+    bgSubFrameNum += 1
+    while (1) {
+        frame = bgSub.read()
+        if (frame.empty) {
+            break
+        }
+        frame = frame.cvtColor(cv2.COLOR_BGR2GRAY)
+        bgSubFrameNum += 1
+        bgSubFrames = Buffer.concat([bgSubFrames, frame.getData()])
+    }
+}
 const app = new Koa();
 app.use(cors());
 function render(filename) {
@@ -13,7 +35,7 @@ function render(filename) {
     return fs.readFileSync(fullpath, 'binary')
 }
 
-/** 
+/**
 在此可組合各種 Middleware
 **/
 app.use(async (ctx) => {
@@ -29,6 +51,12 @@ app.use(async (ctx) => {
             break
         case "/test":
             ctx.body = JSON.stringify({ hello: "world" })
+            break
+        case "/q1-size":
+            ctx.body = JSON.stringify({ h: bgSubH, w: bgSubW, frameNum: bgSubFrameNum })
+            break
+        case "/q1-img":
+            ctx.body = bgSubFrames
             break
     }
 })
--- cvdl2020/hw2/dev/test.js
+++ cvdl2020/hw2/dev/test.js
@@ -1,9 +1,23 @@
 import * as tf from '@tensorflow/tfjs'
-console.log(tf)
-fetch("http://localhost:3000/test")
-    .then(function (response) {
-        return response.json();
+let canvas = document.createElement('canvas')
+document.body.appendChild(canvas)
+
+fetch("http://localhost:3000/q1-size")
+    .then((response) => {
+        return response.json()
     })
-    .then(function (myJson) {
-        console.log(myJson);
-    });
+    .then((size) => {
+        fetch("http://localhost:3000/q1-img")
+            .then((response) => {
+                return response.arrayBuffer();
+            })
+            .then((buf) => {
+                let imgs = tf.tensor(new Uint8Array(buf), [size.frameNum, size.h, size.w, 1])
+                let frame1_50 = imgs.slice([0, 0, 0, 0], [50, -1, -1, -1])
+                let mean = frame1_50.mean(0, true)
+                let std = tf.tidy(() => frame1_50.sub(mean).square().mean(0, true).sqrt().maximum(5))
+                let fg = imgs.sub(mean).abs().greater(std.mul(5)).cast("float32")
+                console.log(frame1_50)
+                tf.browser.toPixels(fg.slice([70, 0, 0, 0], [1, -1, -1, -1]).squeeze(0), canvas)
+            });
+    })
