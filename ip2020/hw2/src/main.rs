7c8e312e9b7810922beb37d76292374b401ef9d1
toonnyy8 <toonnyy8@gmail.com>
2021-01-10 14:50:50 +0800

    update

--- ip2020/hw2/src/main.rs
+++ ip2020/hw2/src/main.rs
@@ -54,6 +54,7 @@ struct Counter {
     model: tch::CModule,
     label: tch::Tensor,
     inp: tch::Tensor,
+    segment: bool,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -68,7 +69,7 @@ fn load_img_tensor(path: std::string::String) -> tch::Tensor {
     let t2 = tch::vision::image::load(path);
     let t2 = match t2 {
         Ok(t2) => t2.slice(0, 0, 1, 1),
-        Err(_) => tch::Tensor::zeros(&[1, 512, 512], (tch::Kind::Int64, tch::Device::Cpu)),
+        Err(_) => tch::Tensor::zeros(&[1, 256, 256], (tch::Kind::Int64, tch::Device::Cpu)),
     };
     t2
 }
@@ -105,6 +106,7 @@ impl Sandbox for Counter {
             model: model,
             label: tch::Tensor::zeros(&[0], (tch::Kind::Float, tch::Device::Cpu)),
             inp: tch::Tensor::zeros(&[0], (tch::Kind::Float, tch::Device::Cpu)),
+            segment: false,
         }
     }
 
@@ -141,11 +143,7 @@ impl Sandbox for Counter {
                 }
             }
             Message::SegmentationPressed => {
-                let pred = self.model.forward_ts(&[self.inp.unsqueeze(0)]);
-                let pred = match pred {
-                    Ok(pred) => (pred.squeeze().round() * 255) as tch::Tensor,
-                    Err(e) => panic!("error: {:?}", e),
-                };
+                self.segment = true;
             }
             Message::TestMsg(idx) => {
                 self.file_idx = idx as usize;
@@ -154,7 +152,6 @@ impl Sandbox for Counter {
     }
 
     fn view(&mut self) -> Element<Message> {
-        println!("15");
         let mut ctrl = Row::new();
         ctrl = ctrl
             .push(
@@ -168,59 +165,116 @@ impl Sandbox for Counter {
                     .style(style::Button::Primary),
             );
 
-        let mut alpha = tch::Tensor::ones(&[1, 512, 512], (tch::Kind::Float, tch::Device::Cpu));
+        let mut alpha = tch::Tensor::ones(&[1, 256, 256], (tch::Kind::Float, tch::Device::Cpu));
         let alpha = tch::Tensor::fill_(&mut alpha, 255);
 
         let t1 = load_img_tensor(format!(
             "{}/T1/{}.jpg",
             self.dir_names[self.dir_idx], self.file_idx,
         ));
+        let t1 = t1.slice(1, 128, 384, 1);
+        let t1 = t1.slice(2, 128, 384, 1);
         let t2 = load_img_tensor(format!(
             "{}/T2/{}.jpg",
             self.dir_names[self.dir_idx], self.file_idx,
         ));
+        let t2 = t2.slice(1, 128, 384, 1);
+        let t2 = t2.slice(2, 128, 384, 1);
         self.inp = tch::Tensor::cat(&[t1.copy(), t2.copy()], 0) / 255;
 
         let ct = load_img_tensor(format!(
             "{}/CT/{}.jpg",
             self.dir_names[self.dir_idx], self.file_idx,
         ));
+        let ct = ct.slice(1, 128, 384, 1);
+        let ct = ct.slice(2, 128, 384, 1);
         let ft = load_img_tensor(format!(
             "{}/FT/{}.jpg",
             self.dir_names[self.dir_idx], self.file_idx,
         ));
+        let ft = ft.slice(1, 128, 384, 1);
+        let ft = ft.slice(2, 128, 384, 1);
         let mn = load_img_tensor(format!(
             "{}/MN/{}.jpg",
             self.dir_names[self.dir_idx], self.file_idx,
         ));
+        let mn = mn.slice(1, 128, 384, 1);
+        let mn = mn.slice(2, 128, 384, 1);
         self.label = tch::Tensor::cat(&[ct.copy(), ft.copy(), mn.copy()], 0) / 255;
-        let mask = self.label.copy() * 0.5;
+        let label_mask = self.label.copy() * 0.5;
 
-        let t1_img: tch::Tensor = t1.repeat(&[3, 1, 1]) * (1 - mask.copy()) + mask.copy() * 255;
-        let t1_img = t1_img.round();
-        let t1_img = tch::Tensor::cat(&[t1_img.copy(), alpha.copy()], 0);
-        let t1_img: tch::Tensor = t1_img.to_kind(tch::Kind::Uint8);
-        let t1_img = t1_img.movedim(&[0], &[2]);
-        let t1_imgh = image::Handle::from_pixels(512, 512, Vec::<u8>::from(&t1_img));
+        let label_t1_img: tch::Tensor =
+            t1.repeat(&[3, 1, 1]) * (1 - label_mask.copy()) + label_mask.copy() * 255;
+        let label_t1_img = label_t1_img.round();
+        let label_t1_img = tch::Tensor::cat(&[label_t1_img.copy(), alpha.copy()], 0);
+        let label_t1_img: tch::Tensor = label_t1_img.to_kind(tch::Kind::Uint8);
+        let label_t1_img = label_t1_img.movedim(&[0], &[2]);
+        let label_t1_imgh = image::Handle::from_pixels(256, 256, Vec::<u8>::from(&label_t1_img));
 
-        let t2_img: tch::Tensor = t2.repeat(&[3, 1, 1]) * (1 - mask.copy()) + mask.copy() * 255;
-        let t2_img = t2_img.round();
-        let t2_img = tch::Tensor::cat(&[t2_img.copy(), alpha.copy()], 0);
-        let t2_img: tch::Tensor = t2_img.to_kind(tch::Kind::Uint8);
-        let t2_img = t2_img.movedim(&[0], &[2]);
-        let t2_imgh = image::Handle::from_pixels(512, 512, Vec::<u8>::from(&t2_img));
+        let label_t2_img: tch::Tensor =
+            t2.repeat(&[3, 1, 1]) * (1 - label_mask.copy()) + label_mask.copy() * 255;
+        let label_t2_img = label_t2_img.round();
+        let label_t2_img = tch::Tensor::cat(&[label_t2_img.copy(), alpha.copy()], 0);
+        let label_t2_img: tch::Tensor = label_t2_img.to_kind(tch::Kind::Uint8);
+        let label_t2_img = label_t2_img.movedim(&[0], &[2]);
+        let label_t2_imgh = image::Handle::from_pixels(256, 256, Vec::<u8>::from(&label_t2_img));
 
-        let label_img_container = Row::new()
+        let label_img_container = Column::new()
             .push(
-                Container::new(Image::new(t1_imgh))
-                    .width(Length::Units(500))
+                Container::new(Image::new(label_t1_imgh))
+                    .width(Length::Units(256))
                     .center_x(),
             )
             .push(
-                Container::new(Image::new(t2_imgh))
-                    .width(Length::Units(500))
+                Container::new(Image::new(label_t2_imgh))
+                    .width(Length::Units(256))
                     .center_x(),
             );
+
+        let pred_img_container = if self.segment {
+            self.segment = false;
+            let pred = self.model.forward_ts(&[self.inp.unsqueeze(0)]);
+            let pred = match pred {
+                Ok(pred) => pred.squeeze().round(),
+                Err(e) => panic!("error: {:?}", e),
+            };
+            let pred_mask = pred.copy() * 0.5;
+
+            let pred_t1_img: tch::Tensor =
+                t1.repeat(&[3, 1, 1]) * (1 - pred_mask.copy()) + pred_mask.copy() * 255;
+            let pred_t1_img = pred_t1_img.round();
+            let pred_t1_img = tch::Tensor::cat(&[pred_t1_img.copy(), alpha.copy()], 0);
+            let pred_t1_img: tch::Tensor = pred_t1_img.to_kind(tch::Kind::Uint8);
+            let pred_t1_img = pred_t1_img.movedim(&[0], &[2]);
+            let pred_t1_imgh = image::Handle::from_pixels(256, 256, Vec::<u8>::from(&pred_t1_img));
+
+            let pred_t2_img: tch::Tensor =
+                t2.repeat(&[3, 1, 1]) * (1 - pred_mask.copy()) + pred_mask.copy() * 255;
+            let pred_t2_img = pred_t2_img.round();
+            let pred_t2_img = tch::Tensor::cat(&[pred_t2_img.copy(), alpha.copy()], 0);
+            let pred_t2_img: tch::Tensor = pred_t2_img.to_kind(tch::Kind::Uint8);
+            let pred_t2_img = pred_t2_img.movedim(&[0], &[2]);
+            let pred_t2_imgh = image::Handle::from_pixels(256, 256, Vec::<u8>::from(&pred_t2_img));
+            Column::new()
+                .push(
+                    Container::new(Image::new(pred_t1_imgh))
+                        .width(Length::Units(256))
+                        .center_x(),
+                )
+                .push(
+                    Container::new(Image::new(pred_t2_imgh))
+                        .width(Length::Units(256))
+                        .center_x(),
+                )
+        } else {
+            Column::new()
+                .push(Text::new("").size(10))
+                .push(Text::new("").size(10))
+        };
+
+        let main_container = Row::new()
+            .push(label_img_container)
+            .push(pred_img_container);
 
         Column::new()
             .padding(20)
@@ -237,12 +291,11 @@ impl Sandbox for Counter {
                 .step(1),
             )
             .push(Text::new(self.file_idx.to_string()).size(24))
-            .push(label_img_container)
+            .push(main_container)
             .push(
                 Button::new(&mut self.segmentation_button, Text::new("segmentation"))
                     .on_press(Message::SegmentationPressed),
             )
-            .height(Length::Units(800))
             .into()
     }
 }
