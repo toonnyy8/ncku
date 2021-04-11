810f2fbd3cf7eb705c9a20c936834a3f8d69a7ab
toonnyy8 <toonnyy8@gmail.com>
2021-04-11 20:08:51 +0800

    update

--- cgap2021/hw2/dev/src/App.vue
+++ cgap2021/hw2/dev/src/App.vue
@@ -1,14 +1,45 @@
 <template>
-    <canvas
-        class="float-left max-w-7xl"
-        ref="canvasRef"
-        width="800"
-        height="600"
-        v-on:mousedown="mousedown"
-        v-on:mousemove="mousemove"
-        v-on:mouseup="mouseup"
-    ></canvas>
-    <div class="text-red-500 float-left max-w-7xl">
+    <div class="text-gray-800 float-left">
+        <canvas
+            v-show="mode == 'source'"
+            class="max-w-7xl"
+            ref="srcCanvasRef"
+            width="800"
+            height="600"
+            v-on:mousedown="srcMousedown"
+            v-on:mousemove="srcMousemove"
+            v-on:mouseup="srcMouseup"
+        ></canvas>
+        <canvas
+            v-show="mode == 'target'"
+            class="max-w-7xl"
+            ref="tarCanvasRef"
+            width="800"
+            height="600"
+        ></canvas>
+        <br />
+        <button
+            v-bind:class="{
+                'bg-blue-300': mode != 'source',
+                'bg-yellow-300': mode == 'source',
+            }"
+            class="py-1 px-5 rounded-b-lg"
+            v-on:click="activatedMode('source')"
+        >
+            Source
+        </button>
+        <button
+            v-bind:class="{
+                'bg-blue-300': mode != 'target',
+                'bg-yellow-300': mode == 'target',
+            }"
+            class="py-1 px-5 rounded-b-lg"
+            v-on:click="activatedMode('target')"
+        >
+            target
+        </button>
+    </div>
+    <div class="text-gray-800">
         from: ({{ line.from.x }}, {{ line.from.y }})
         <br />
         to: ({{ line.to.x }}, {{ line.to.y }})
@@ -16,11 +47,23 @@
         <ul>
             <li v-for="(value, name) in lines" :key="name">
                 {{ name }}
-                <div class="flex justify-start ...">
-                    <div class="bg-purple-600 w-10 h-10 float-left rounded-l-lg"></div>
-                    <div class="float-left">
+                <div class="flex justify-start w-72">
+                    <div
+                        v-on:click="lookLine(name)"
+                        v-bind:class="{
+                            'bg-blue-300': name != lookLineIdx,
+                            'bg-yellow-300': name == lookLineIdx,
+                        }"
+                        class="py-1 px-3 w-full rounded-l-lg"
+                    >
                         from: ({{ value.from.x }}, {{ value.from.y }}) to: ({{ value.to.x }},
                         {{ value.to.y }})
+                    </div>
+                    <div
+                        v-on:click="removeLine(name)"
+                        class="bg-pink-500 p-1 w-10 rounded-r-lg text-center"
+                    >
+                        <img :src="close_icon" alt="remove" />
                     </div>
                 </div>
             </li>
@@ -30,6 +73,8 @@
 
 <script lang="ts">
 import { defineComponent, ref, onMounted, Ref } from "vue"
+// @ts-ignore
+import close_icon from "url:../icon/highlight_off_black_24dp.svg"
 interface Point {
     x: number
     y: number
@@ -64,53 +109,115 @@ const drawLine = (ctx: CanvasRenderingContext2D, line: Line, color: string = `#f
 
 export default defineComponent({
     setup() {
-        const canvasRef: Ref<HTMLCanvasElement> = ref()
-        const mousedown = ref((e: MouseEvent) => {})
-        const mousemove = ref((e: MouseEvent) => {})
-        const mouseup = ref((e: MouseEvent) => {})
+        const srcCanvasRef: Ref<HTMLCanvasElement> = ref()
+        const srcMousedown = ref((e: MouseEvent) => {})
+        const srcMousemove = ref((e: MouseEvent) => {})
+        const srcMouseup = ref((e: MouseEvent) => {})
         const line: Ref<Line> = ref({ from: { x: 0, y: 0 }, to: { x: 0, y: 0 } })
         const lines: Ref<Line[]> = ref([])
+        const _lines: Ref<{ src: Line; tar: Line }[]> = ref([])
+
+        const lookLineIdx = ref(-1)
+        const lookLine = ref((idx: number) => {})
+
+        const mode = ref("source")
+        const activatedMode = ref((_mode: string) => (mode.value = _mode))
+
+        const removeLine = ref((idx: number) => {
+            lines.value = lines.value.reduce((lineArr, currLine, currIdx) => {
+                if (currIdx != idx) {
+                    lineArr.push(currLine)
+                }
+                return lineArr
+            }, [])
+            let lookIdx = lookLineIdx.value
+            lookLineIdx.value = -1
+            if (lookIdx != -1) {
+                if (lookIdx == idx) {
+                    lookIdx = -1
+                } else if (lookIdx > idx) {
+                    lookIdx -= 1
+                }
+            }
+
+            lookLine.value(lookIdx)
+        })
 
         onMounted(() => {
-            const canvas = canvasRef.value
-            const ctx = canvas.getContext("2d")
+            const srcCanvas = srcCanvasRef.value
+            const srcCtx = srcCanvas.getContext("2d")
 
             let down = false
-            mousedown.value = (e) => {
+            srcMousedown.value = (e) => {
                 down = true
                 line.value = {
                     from: {
-                        x: e.pageX - canvas.offsetLeft,
-                        y: e.pageY - canvas.offsetTop,
+                        x: e.pageX - srcCanvas.offsetLeft,
+                        y: e.pageY - srcCanvas.offsetTop,
                     },
                     to: {
-                        x: e.pageX - canvas.offsetLeft,
-                        y: e.pageY - canvas.offsetTop,
+                        x: e.pageX - srcCanvas.offsetLeft,
+                        y: e.pageY - srcCanvas.offsetTop,
                     },
                 }
-                ctx.clearRect(0, 0, 800, 600)
-                ;[...lines.value, line.value].forEach((line) => drawLine(ctx, line))
+                srcCtx.clearRect(0, 0, 800, 600)
+                lines.value.forEach((line) => drawLine(srcCtx, line, `#93c5fd`))
+                drawLine(srcCtx, line.value)
             }
-            mousemove.value = (e: MouseEvent) => {
+            srcMousemove.value = (e: MouseEvent) => {
                 if (down) {
                     line.value.to = {
-                        x: e.pageX - canvas.offsetLeft,
-                        y: e.pageY - canvas.offsetTop,
+                        x: e.pageX - srcCanvas.offsetLeft,
+                        y: e.pageY - srcCanvas.offsetTop,
                     }
-                    ctx.clearRect(0, 0, 800, 600)
-                    ;[...lines.value, line.value].forEach((line) => drawLine(ctx, line))
+                    srcCtx.clearRect(0, 0, 800, 600)
+                    lines.value.forEach((line) => drawLine(srcCtx, line, `#93c5fd`))
+                    drawLine(srcCtx, line.value)
                 }
             }
-            mouseup.value = (e) => {
+            srcMouseup.value = (e) => {
                 down = false
-                if (line.value.from.x != line.value.to.x || line.value.from.y != line.value.to.y)
+                srcCtx.clearRect(0, 0, 800, 600)
+                lines.value.forEach((line) => drawLine(srcCtx, line, `#93c5fd`))
+                if (line.value.from.x != line.value.to.x || line.value.from.y != line.value.to.y) {
+                    drawLine(srcCtx, line.value)
                     lines.value.push(Object.assign({}, line.value))
+                    lookLineIdx.value = lines.value.length - 1
+                }
+            }
 
-                ctx.clearRect(0, 0, 800, 600)
-                lines.value.forEach((line) => drawLine(ctx, line))
+            lookLine.value = (idx: number) => {
+                if (lookLineIdx.value == idx) {
+                    lookLineIdx.value = -1
+                } else {
+                    lookLineIdx.value = idx
+                }
+                srcCtx.clearRect(0, 0, 800, 600)
+                lines.value.forEach((line, i) => {
+                    if (i != lookLineIdx.value) {
+                        drawLine(srcCtx, line, `#93c5fd`)
+                    } else {
+                        drawLine(srcCtx, line)
+                    }
+                })
+                console.log(idx)
             }
         })
-        return { mousedown, mousemove, mouseup, canvasRef, line, lines }
+
+        return {
+            srcMousedown,
+            srcMousemove,
+            srcMouseup,
+            srcCanvasRef,
+            line,
+            lines,
+            close_icon,
+            lookLine,
+            lookLineIdx,
+            removeLine,
+            mode,
+            activatedMode,
+        }
     },
 })
 </script>
