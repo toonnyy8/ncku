9c4f079f13552cf6c02abc4b4c2e486c8e561499
toonnyy8 <toonnyy8@gmail.com>
2021-04-29 16:59:18 +0800

    update

---
+++ dev/hw3/index.ts
@@ -0,0 +1,4 @@
+import * as glm from "gl-matrix"
+const hi: string = "hi"
+console.log(hi)
+console.log(glm)
---
+++ out.js
@@ -0,0 +1,5 @@
+(() => {
+  // cgap2021/hw3/src/index.ts
+  var hi = "hi";
+  console.log(hi);
+})();
